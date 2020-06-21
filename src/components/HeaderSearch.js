8e1a697d81d244d75f85c0bddcba460c26bf0d97
Jaydeep Deb <jaydeep.jaydeep.deb@gmail.com>
2020-06-21 18:18:33 +0530

    Added materialize npm library for js components

--- src/components/HeaderSearch.js
+++ src/components/HeaderSearch.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import M from 'materialize-css'
 
 export default function HeaderSearch() {
+
+    useEffect(() => {
+        M.Dropdown.init(document.querySelector('.dropdown-trigger'), {});
+    }, [])
+
     return (
         <nav className="header-search nav-wrapper indigo">
             <div className="container">
                 <a className='dropdown-trigger btn yellow' style={{color: 'black', borderRadius: 'none'}} href='#' data-target='dropdown1'>RENT</a>
-                <ul id='dropdown1' class='dropdown-content'>
+                <ul id='dropdown1' className='dropdown-content'>
                     <li><a href="#!">one</a></li>
                     <li><a href="#!">two</a></li>
                 </ul>
