f8041168c53a33ba9a5ce0e4468993602a754d16
skidi3 <43917504+skidi3@users.noreply.github.com>
2020-06-26 20:30:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="form-wrapper">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
